5252f277d85025427b38a6e16f62f05e2dbc9cd5
Mintaek2 <789rkd@naver.com>
2023-11-09 20:00:26 +0900

    feat : addTodo, change Todo state

--- TodoList/Todo.js
+++ TodoList/Todo.js
@@ -1,3 +1,37 @@
-function addTodo(){
-    console.log('add-todo')
+// ADD TODO
+let todoInput = document.getElementById("new-todo");
+let addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", addTodo);
+
+function addTodo() {
+  const newTodoText = todoInput.value;
+  if (newTodoText != "") {
+    const todoList = document.querySelector(".todo-item");
+    const newTodoItem = document.createElement("li");
+    newTodoItem.innerHTML = `
+        <input type='checkbox'>
+        <label>${newTodoText}</label>
+    `;
+    todoList.appendChild(newTodoItem);
+
+    newTodoText.value = "";
+  }
+}
+
+// Change item state
+document.addEventListener("change", changeItem);
+function changeItem(e) {
+  if (e.target.type === "checkbox") {
+    const todoItem = e.target.parentElement;
+    if (e.target.checked) {
+      const completedList = document.querySelector(".completed-list");
+      completedList.appendChild(todoItem);
+
+      todoItem.classList.add("completed");
+    } else {
+      const todoList = document.querySelector(".todo-list");
+      todoList.appendChild(todoItem);
+      todoItem.classList.remove("completed");
+    }
+  }
 }
